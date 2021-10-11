f802424e7fc38e6e54a58c7d45b6d987961e1565
shaikhmohseen <mohseen.shaikh@medlypharmacy.com>
2021-10-11 21:17:59 +0530

    testing using aws cli

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-resource "aws_instance" "Linux" {
+    resource "aws_instance" "Linux" {
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
   key_name               = var.key_name
